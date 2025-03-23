2fe3e914a254f47a7626b8e73df3683f3b4261b6
zeph1rr <grianton535@gmail.com>
2025-03-24 00:44:49 +0300

    removed md files, changed SceneManager to static

--- README.md
+++
@@ -1,1 +0,0 @@
-# Zeph1rrGameBase
--- Scripts/Core/Scene/SceneManager.cs
+++ Scripts/Core/Scene/SceneManager.cs
@@ -3,23 +3,16 @@ using System.Collections;
 
 namespace Zeph1rrGameBase.Scripts.Core.Scene
 {
-    public class SceneManager
+    public static class SceneManager
     {
-        private bool _sceneLoading;
+        private static bool _sceneLoading;
 
-        private readonly string[] _sceneNames;
-
-        public SceneManager(string[] sceneNames)
-        {
-            _sceneNames = sceneNames;
-        }
-
-        public IEnumerator LoadSceneAsync(string sceneName, Action callback)
+        public static IEnumerator LoadSceneAsync(string sceneName, Action callback)
         {
             if (_sceneLoading) yield break;
             _sceneLoading = false;
             yield return UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneName);
-            var scene = new Core.Scene.Scene(sceneName);
+            var scene = new Scene(sceneName);
             scene.StartScene();
             callback.Invoke();
             _sceneLoading = true;
